cd2454856ddcfe0ad0ec0f0cf2c1350bbc2c7606
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-05-19 14:19:19 +0600

    database connection successfull

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,47 @@
 const express = require('express');
 const env = require('dotenv');
-
 const app = express()
+const mongoose = require('mongoose')
 
 //Configuartion environment variable
 
 env.config();
 
-app.get('/', (req,res,next) => {
+//Configure middleware
+app.use(express.urlencoded({ extended: true })),
+    app.use(express.json()),
+
+    //app.use(express.json())
+    // app.use(urlencoded)
+    // bodyParser.json()
+    //app.use(bodyParser.urlencoded({ extended: false }))
+
+    app.post('/data', (req, res, next) => {
+        console.log("jj", req.body)
+        res.status(200).json({
+            message: req.body,
+        })
+    })
+app.get('/', (req, res, next) => {
     console.log("jj")
     res.status(200).json({
         message: "Hello!, this message from the server",
     })
 })
-//Run the server
 
-app.listen(process.env.PORT || 8800, (req,res)=> {
+// Create database
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Database Created successfully")
+    // app.listen(PORT,()=> {
+    //     console.log(`Server is running on port ${PORT}`)
+    // })
+}).catch(err => {
+    console.log(err)
+})
+//Run the server
+app.listen(process.env.PORT || 8800, (req, res) => {
     console.log(`server is running on the port ${process.env.PORT}`)
 })
