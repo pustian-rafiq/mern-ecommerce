742457ddf8c28b2dfe61be94b1f49d906399326d
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-05-26 15:45:08 +0600

    fetch all categories api done

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,30 +1,11 @@
 const router = require('express').Router();
 const slugify = require('slugify');
-const Category = require('../models/category');
+const { addCategoryController, getAllCategories } = require('../controllers/categoryController');
 
 
-router.post('/category/create',(req, res) => {
 
-    console.log(req.body.name)
-    const categoryObject = {
-        name: req.body.name,
-        slug: slugify(req.body.name)
-    };
-
-    if(req.body.parentId){
-        categoryObject.parentId = req.body.parentId;
-    }
-
-    const newCategory = Category(categoryObject);
-
-    newCategory.save((error,category) => {
-        if(error) return res.status(400).json({err})
-        if(category){
-            res.status(201).json({category})
-        }
-    })
-
-})
+router.post('/category/create',addCategoryController);
+router.get('/category/all',getAllCategories);
 
 
  
