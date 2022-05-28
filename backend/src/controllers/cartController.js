6c09edb0a121cf70d46da78bbb932becd32db83f
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-05-28 14:39:16 +0600

    cart add product done

--- backend/src/controllers/cartController.js
+++ backend/src/controllers/cartController.js
@@ -2,8 +2,23 @@ const Product = require('../models/product')
 const slugify = require("slugify");
 const shortid = require('shortid');
 
+const Cart = require('../models/cart');
+const product = require('../models/product');
+
 exports.addtoCartController = (req,res) => {
 
-res.status(200).json("cart")
+    const newCart = new Cart({
+        user: req.user._id,
+        cartItems: req.body.cartItems
+    })
+
+    newCart.save((error,cart) => {
+        if(error) return res.status(400).json({error})
+
+        if(cart){
+            return res.status(201).json({cart})
+        }
+    })
+
     
 }
