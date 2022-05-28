139a3faecd4039c861cffce11f664773052c21ef
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-05-28 18:32:15 +0600

    add to cart

--- backend/src/controllers/cartController.js
+++ backend/src/controllers/cartController.js
@@ -6,17 +6,20 @@ exports.addtoCartController = (req, res) => {
 
     // One user can have one card and multiple products, but one user can't have more than one card
     // If he tries to add more card, the existing card wil be returned
-
+    console.log("user id", req.user._id)
     Cart.find({ user: req.user._id })
         .exec((error, cart) => {
             if (error) return res.status(400).json({ error })
-
-            if (cart) {
+            if (cart.length > 0) {
                 // if the cart already exist, we can only update our cart
                 // return res.status(200).json({cart})
-
+                console.log("cart", cart)
+                console.log("cart", req.body.cartItems.product)
                 const product = req.body.cartItems.product;
-                const isItemAdded = cart.cartItems.find(c => c.product === product)
+                const isItemAdded = (cart[0].cartItems).find((c) => {
+                    console.log(c)
+                    c.product === product
+                })
 
                 if (isItemAdded) {
                     Cart.findOneAndUpdate({
@@ -39,8 +42,8 @@ exports.addtoCartController = (req, res) => {
                         })
                 } else {
                     Cart.findOneAndUpdate({ user: req.user._id }, {
-                        "#push": {
-                            "cartItems": [req.body.cartItems]
+                        "$push": {
+                            "cartItems": req.body.cartItems
                         }
                     }).exec((error, _cart) => {
                         if (error) return res.status(400).json({ error })
